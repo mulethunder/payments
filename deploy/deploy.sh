0af77e9215957d1b3f37014be49f8d757b112748
Carlos Rodriguez Iturria <citurria@citurri-ltmhjkb.internal.salesforce.com>
2022-09-06 15:25:04 +1000

    Adding changes to Payments microservice

---
+++ deploy/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#microservices..
+  #payments microservice..
+  #git clone --quiet https://github.com/mulethunder/payments.git >>/tmp/noise.out && cd payments
+  kubectl create namespace payments
+  
+  ## Updating as Conatiner repo is public now:
+  # kubectl create secret docker-registry dhreg --docker-server=https://index.docker.io/v1/ --docker-username=XXXXXX --docker-password=XXXXX --docker-email=XXXX --namespace=payments
+
+
+  kubectl create -f kubernetes/payments-dpl.yaml
+  kubectl create -f kubernetes/payments-svc.yaml
+  #kubectl create -f kubernetes/payments-ing.yaml
+
